17a1b619c5dbc6f20c594fda93091efd6362e67c
Hamed AlShukri <hamed@emushrif.om>
2023-09-07 00:55:49 +0400

    Basic project setup

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+// .prettierrc.js
+module.exports = {
+  // Commas & semicolons
+  semi: true,
+  trailingComma: 'es5',
+
+  // Whitespaces
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+
+  singleQuote: true,
+
+  // Functions
+  arrowParens: 'avoid',
+
+  jsxSingleQuote: false,
+
+  bracketSameLine: true,
+
+  singleAttributePerLine: true,
+};
