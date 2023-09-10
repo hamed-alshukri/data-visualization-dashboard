947c258cf7132b3a9a98484ad3f301560c4368fb
Hamed AlShukri <hamed@emushrif.om>
2023-09-10 21:53:56 +0400

    Add fullscreen button

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT, CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +10,64 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  element: any;
+  isFullScreen: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: any) {}
+
+  ngOnInit(): void {
+    this.element = document.documentElement;
+  }
+
+  openFullscreen() {
+    if (this.element.requestFullscreen) {
+      this.element.requestFullscreen();
+    } else if (this.element.mozRequestFullScreen) {
+      /* Firefox */
+      this.element.mozRequestFullScreen();
+    } else if (this.element.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.element.webkitRequestFullscreen();
+    } else if (this.element.msRequestFullscreen) {
+      /* IE/Edge */
+      this.element.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
+  }
+
+  @HostListener('document:fullscreenchange', ['$event'])
+  @HostListener('document:webkitfullscreenchange', ['$event'])
+  @HostListener('document:mozfullscreenchange', ['$event'])
+  @HostListener('document:MSFullscreenChange', ['$event'])
+  fullScreenMode() {
+    this.checkScreenMode();
+  }
+
+  checkScreenMode() {
+    if (document.fullscreenElement) {
+      //fullscreen
+      this.isFullScreen = true;
+    } else {
+      //not in full screen
+      this.isFullScreen = false;
+    }
+  }
+}
